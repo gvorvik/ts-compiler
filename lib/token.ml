49e9a5de7a9190b352fcb29cc0d0d07c89682047
gvorvik <gvorvik@gmail.com>
2024-04-04 20:19:57 -0500

    initialize token helper module

--- lib/token.ml
+++ lib/token.ml
@@ -10,3 +10,13 @@ type t =
   | Equal
   (* Delimiters *)
   | Semi
+
+(* Helpers for building lexical tokens *)
+module TokenHelpers = struct
+  let is_number = function '0' .. '9' -> true | _ -> false
+  let is_letter = function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false
+  let acceptable_chars = function '_' | '-' -> true | _ -> false
+
+  let is_valid_char x =
+    x |> is_number || x |> is_letter || x |> acceptable_chars
+end
