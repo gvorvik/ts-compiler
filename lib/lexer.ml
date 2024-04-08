cb21fa6cdc1c1d9c23b97fe0f82efbfb3ebaa8fb
gvorvik <gvorvik@gmail.com>
2024-04-08 08:55:51 -0500

    add peek_token and show_token

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -1,6 +1,4 @@
-open Token
-
-type lexer = { lex_buffer : bytes; lex_buffer_len : int; lex_curr_pos : int }
+type t = { lex_buffer : bytes; lex_buffer_len : int; lex_curr_pos : int }
 
 let is_number = function '0' .. '9' -> true | _ -> false
 let is_letter = function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false
@@ -11,7 +9,7 @@ let is_valid_char x =
 
 let string_of_char c = String.make 1 c
 let inc_lex_curr_pos l n = { l with lex_curr_pos = l.lex_curr_pos + n }
-let filter_key_words = function "const" -> Const | t -> Iden t
+let filter_key_words = function "const" -> Token.Const | t -> Token.Iden t
 
 let rec read_word l t =
   if
@@ -29,17 +27,17 @@ let rec read_number l t =
   then
     let char = Bytes.get l.lex_buffer l.lex_curr_pos in
     read_number (inc_lex_curr_pos l 1) (t ^ string_of_char char)
-  else (t, Int (int_of_string t))
+  else (t, Token.Int (int_of_string t))
 
 let build_token l =
   if l.lex_curr_pos < l.lex_buffer_len then
     match Bytes.get l.lex_buffer l.lex_curr_pos with
-    | '+' -> ("+", Add)
-    | '=' -> ("=", Equal)
-    | ';' -> (";", Semi)
+    | '+' -> ("+", Token.Add)
+    | '=' -> ("=", Token.Equal)
+    | ';' -> (";", Token.Semi)
     | '0' .. '9' -> read_number l ""
     | _ -> read_word l ""
-  else ("", EOF)
+  else ("", Token.EOF)
 
 let of_string s =
   {
@@ -59,4 +57,15 @@ let next_token l =
   let l = traverse_white_space l in
   let token = build_token l in
   let token_length = token |> fst |> String.length in
-  (inc_lex_curr_pos l token_length, token)
+  (inc_lex_curr_pos l token_length, token |> snd)
+
+let peek_token l = l |> build_token |> snd
+
+let show_token = function
+  | Token.Add -> "Add"
+  | Token.Const -> "Const"
+  | Token.EOF -> "EOF"
+  | Token.Equal -> "Equal"
+  | Token.Iden s -> "Iden " ^ s
+  | Token.Int i -> "Int " ^ string_of_int i
+  | _ -> "Another Token"
